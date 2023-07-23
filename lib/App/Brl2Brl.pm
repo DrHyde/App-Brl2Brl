62f50373337f3700b3f71246cf7f8adcb03d7268
Lars Bjørndal <lars@lamasti.net>
2023-07-23 17:16:20 +0200

    Updated version number.

--- lib/App/Brl2Brl.pm
+++ lib/App/Brl2Brl.pm
@@ -18,11 +18,11 @@ App::Brl2Brl - Convert between braille display tables defined in Liblouis.
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 
 =head1 SYNOPSIS
